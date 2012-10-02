b7c94e0165935c9dfa3f30f1bd5511d7bc458e4a
Marco Ziener <mziener@lavabit.com>
2012-10-02 19:08:41 +0200

    tree transformation

---
+++ operations.py
@@ -0,0 +1,65 @@
+import parser
+import fofTypes as f
+
+transformations = {
+    '=>' : rewriteImplyR,
+    '<=' : rewriteImplyL,
+    '<=>' : rewriteEquiv,
+    '<~>' : rewriteNotEquiv,
+    '~&' : rewriteNotAnd,
+    '~|' : rewriteNotOr,
+
+}
+
+
+def rewriteImplyR(node):
+    node.op = '|'
+    node.terms[0] = f.UnaryOperand('~', node.terms[0])
+
+def rewriteImplyL(node):
+    node.op = '|'
+    node.terms[1] = f.UnaryOperand('~', node.terms[1])
+
+def rewriteEquiv(node):
+    leftnode = f.BinaryOperand('&')
+    leftnode.terms[0] = node.terms[0]
+    leftnode.terms[1] = node.terms[1]
+    rightnode = f.BinaryOperand('&')
+    rightnode.terms[0] = f.UnaryOperand('~')
+    rightnode.terms[0].term = node.terms[0]
+    rightnode.terms[1] = f.UnaryOperand('~')
+    rightnode.terms[1].term = node.terms[1]
+    node = f.BinaryOperand('|')
+    node.terms[0] = leftnode
+    node.terms[1] = rightnode
+
+def rewriteNotEquiv(node):
+    leftnode = f.BinaryOperand('&')
+    rightnode = f.BinaryOperand('&')
+    leftnode.terms[0] = node.terms[0]
+    leftnode.terms[1] = f.UnaryOperand('~')
+    leftnode.terms[1].term = node.terms[1]
+    rightnode.terms[0] = f.UnaryOperand('~')
+    rightnode.terms[0].term = node.terms[0]
+    rightnode.terms[1] = node.terms[1]
+    node = f.BinaryOperand('|')
+    node.terms[0] = leftnode
+    node.terms[1] = rightnode
+
+def rewriteNotOr(node):
+    leftnode = f.UnaryOperand('~')
+    leftnode = node.term[0]
+    rightnode = f.UnaryOperad('~')
+    rightnode = node.term[1]
+    node = f.BinaryOperand('&')
+    node.terms[0] = leftnode
+    node.terms[1] = rightnode
+
+def rewriteNotAnd(node):
+    leftnode = f.UnaryOperand('~')
+    leftnode = node.term[0]
+    rightnode = f.UnaryOperad('~')
+    rightnode = node.term[1]
+    node = f.BinaryOperand('|')
+    node.terms[0] = leftnode
+    node.terms[1] = rightnode
