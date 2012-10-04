05c03f3f2de530f5ff0b376b716019201b470728
Michael Wittig <michael.wittig@fu-berlin.de>
2012-10-04 14:38:06 +0200

    operations updated to new foftypes constructor

--- operations.py
+++ operations.py
@@ -1,56 +1,33 @@
 import parser
 import fofTypes as f
 
-
-
-
 def rewriteImplyR(node):
-    node.op = '|'
-    leftnode = f.UnaryOperand('~')
-    leftnode.term = node.terms[0]
-    node.terms = leftnode, node.terms[1]
+    leftnode = f.UnaryOperand('~', node.terms[0])
+    node = f.BinaryOperand('|', leftnode, node.terms[1])
 
 def rewriteImplyL(node):
-    node.op = '|'
-    rightnode = f.UnaryOperand('~')
-    rightnode.term = node.terms[1]
-    node.terms = node.terms[0],rightnode
+    rightnode = f.UnaryOperand('~', node.terms[1])
+    node = f.BinaryOperand('|', node.terms[0], rightnode)
 
 def rewriteEquiv(node):
-    leftnode = f.BinaryOperand('&')
-    leftnode.terms = node.terms[0],node.terms[1]
-    rightnode = f.BinaryOperand('&')
-    rightnode.terms = f.UnaryOperand('~'),f.UnaryOperand('~')
-    rightnode.terms[0].term = node.terms[0]
-    rightnode.terms[1].term = node.terms[1]
-    node = f.BinaryOperand('|')
-    node.terms = leftnode,rightnode
+    leftnode = f.BinaryOperand('&',node.terms[0],node.terms[1])
+    rightnode = f.BinaryOperand('&',f.UnaryOperand('~',node.terms[0]),f.UnaryOperand('~',node.terms[1]))
+    node = f.BinaryOperand('|',leftnode,rightnode)
 
 def rewriteNotEquiv(node):
-    leftnode = f.BinaryOperand('&')
-    rightnode = f.BinaryOperand('&')
-    leftnode.terms = node.terms[0],f.UnaryOperand('~')
-    leftnode.terms[1].term = node.terms[1]
-    rightnode.terms = f.UnaryOperand('~'),node.terms[1]
-    rightnode.terms[0].term = node.terms[0]
-    node = f.BinaryOperand('|')
-    node.terms = leftnode,rightnode
+    leftnode = f.BinaryOperand('&',node.terms[0],f.UnaryOperand('~',node.terms[1]))
+    rightnode = f.BinaryOperand('&',f.UnaryOperand('~',node.terms[0]),node.terms[1])
+    node = f.BinaryOperand('|',leftnode,rightnode)
 
 def rewriteNotOr(node):
-    leftnode = f.UnaryOperand('~')
-    leftnode.term = node.terms[0]
-    rightnode = f.UnaryOperand('~')
-    rightnode.term = node.terms[1]
-    node = f.BinaryOperand('&')
-    node.terms = leftnode,rightnode
+    leftnode = f.UnaryOperand('~',node.terms[0])
+    rightnode = f.UnaryOperand('~',node.terms[1])
+    node = f.BinaryOperand('&',leftnode,rightnode)
 
 def rewriteNotAnd(node):
-    leftnode = f.UnaryOperand('~')
-    leftnode.term = node.terms[0]
-    rightnode = f.UnaryOperand('~')
-    rightnode.term = node.terms[1]
-    node = f.BinaryOperand('|')
-    node.terms = leftnode,rightnode
+    leftnode = f.UnaryOperand('~',node.terms[0])
+    rightnode = f.UnaryOperand('~',node.terms[1])
+    node = f.BinaryOperand('|',leftnode,rightnode)
 
 transformations = {
     '=>' : rewriteImplyR,
