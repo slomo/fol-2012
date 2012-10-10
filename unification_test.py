18924d10fdc66ae608abab77d2ee7a8e89593da2
Michael Wittig <michael.wittig@fu-berlin.de>
2012-10-10 15:13:55 +0200

    corrected multiple_robinson test

--- unification_test.py
+++ unification_test.py
@@ -7,15 +7,12 @@ from fofTypes import *
 class UnificationTest(unittest.TestCase):
 
     def test_mrs_robinson(self):
-
         f1 = Function("f",[Variable("X")])
         f2 = Function("f",[Variable("Y")])
 	sigma = u.mrs_robinson(f1,f2)
 	f1 = u.substitute(f1,sigma)
 	f2 = u.substitute(f2,sigma)
         self.assertTrue(f1==f2)
-
-
 
     def test_unification(self):
 
@@ -28,7 +25,7 @@ class UnificationTest(unittest.TestCase):
 		newlist.append(u.substitute(x,sigma))
 	print("newlist", newlist)
 	for x in newlist:
-		if x != f2:
+		if not (x == f2):
 			print("x: ", x, "f2", f2)
 			self.assertTrue(False)
 	
