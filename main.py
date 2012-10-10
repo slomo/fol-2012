b3f66ec7bb3266cd90e0151c4cf5a487483ccb1b
yves <steve.harrison@gmx.net>
2012-10-10 08:07:51 +0200

    add new parameter to input fromular directly

--- main.py
+++ main.py
@@ -15,22 +15,21 @@ if __name__ == '__main__':
 
     parser = a.ArgumentParser(description='A simple fof solver using tptp syntax')
     parser.add_argument('--file', action='store')
+    parser.add_argument('--formula', action='store')
+
     args = vars(parser.parse_args())
-    x = 0
     if args['file']:
-        x = from_file(args['file'])
-        print(x)
+        fof_data = from_file(args['file'])
+    elif args['formula']:
+        fof_data = from_string(args['formula'])
     else :
         string = "fof(ax, axiom, ?[X]: r(f(a,X),Y,x) )."
-        print("Input was:", string)
-        x = from_string(string)
+        fof_data = from_string(string)
 
-    tree = x[0]["formula"]
-    print(tree)
-    tree = f.UnaryOperand("~",tree)
-    print("and transformed ...")
-    tree = o.transform(tree)
-    tree = o.transform(tree)
-    print(tree)
-    print("and solved")
-    print(r.proof(tree))
+    formula = fof_data[0]["formula"]
+    print("input formula:",formula)
+    formula = formula.negate()
+    formula = o.transform(formula)
+    print("transform negated formula:", formula)
+    result = r.proof(formula)
+    print("formula holds:", result)
